06d6959cd46d2e2741850fed7cfaf30ce8d59f2c
Haris Alicic <haris.alicic1@gmail.com>
2025-01-21 08:13:21 +0100

    Initial Commit

---
+++ Pomodoro/PomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroApp.swift
+//  Pomodoro
+//
+//  Created by Haris Alicic on 21. 1. 2025..
+//
+
+import SwiftUI
+
+@main
+struct PomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
